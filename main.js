781fee61d09b74b35bf91c2771ba54bb61362435
grace7937 <66059987+grace7937@users.noreply.github.com>
2020-12-16 19:30:25 +0900

    JS:project filtering anim

--- main.js
+++ main.js
@@ -71,13 +71,16 @@ workBtnContainer.addEventListener('click', (e) => {
   if (filter == null) {
     return;
   }
-
-  projects.forEach((project) => {
-    if (filter === '*' || filter === project.dataset.type) {
-      console.log(project.dataset.type);
-      project.classList.remove('invisible');
-    } else {
-      project.classList.add('invisible');
-    }
-  });
+  projectContainer.classList.add('anim-out');
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if (filter === '*' || filter === project.dataset.type) {
+        console.log(project.dataset.type);
+        project.classList.remove('invisible');
+      } else {
+        project.classList.add('invisible');
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
 });
